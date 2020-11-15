e7cb5130ac4907f755066ce3e7616bf1c4c2a403
Yewen Wu <wendy@Yewens-MacBook-Pro.local>
2020-11-14 19:32:48 -0800

    Created LogIn and SignUp page

---
+++ src/components/authentication/LogIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Authentication from "./Authentication";
+
+const LogIn = () =>
+    <Authentication title="Log In" link="Sign Up" message="New to BayBookClub?" url="/signup"/>
+
+export default LogIn;
---
+++ src/components/authentication/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Authentication from "./Authentication";
+
+const SignUp = () =>
+    <Authentication title="Sign Up" link="Log In" message="Already a member?" url="/login"/>
+
+    export default SignUp;
