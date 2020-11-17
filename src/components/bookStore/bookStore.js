3366f66b437eaef0338d61fb4661629345de9065
Chongliang-Utopia <50967591+Chongliang-Utopia@users.noreply.github.com>
2020-11-16 23:57:02 -0500

    bookstore page

--- src/components/bookStore/bookStore.js
+++ src/components/bookStore/bookStore.js
@@ -7,15 +7,55 @@ class BookStore extends React.Component {
     render() {
         return (
             <div className={`${classes.BookStore} container`}>
+                <h4>Our</h4>
+                <h1><b>BOOKSTORE</b></h1>
                 <div className="row">
-                    <div className="col-3">
-                        <div className="filter-panel">
-                            <h4>Filter By</h4>
+                    <div className="col-md-3">
+                        <div className={classes.filterPanel}>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">
+                                    <h5>Filter By</h5>
+                                </li>
+                                <li className="list-group-item">
+                                    <a href="#">
+                                        Newest
+                                    </a>
+                                </li>
+                                <li className="list-group-item">
+                                    <a href="#">
+                                        Most popular
+                                    </a>
+                                </li>
+                                <li className="list-group-item">
+                                    <a href="#">
+                                        Highest rated
+                                    </a>
+                                </li>
+                                <li className="list-group-item">
+                                    <div className="">
+                                        <label htmlFor="formControlRange">
+                                            Minimum Rating:
+                                        </label>
+                                        <input type="range" className="form-control-range"
+                                               min="0" max="5" step="1"
+                                               id="formControlRange"/>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                1 star
+                                            </div>
+                                            <div className="col-6">
+                                                <p className="pull-right">
+                                                    5 stars
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    <div className="col-9">
-                        <h4>Our</h4>
-                        <h1><b>BOOKSTORE</b></h1>
+
+                    <div className="col-md-9">
                         <SearchBook/>
                     </div>
                 </div>
--- src/components/bookStore/searchBook.js
+++ src/components/bookStore/searchBook.js
@@ -1,22 +1,40 @@
 import React from "react"
 import { Link } from "react-router-dom";
-import ImageCard from "../UI/imageCard/ImageCard";
+import ImageCard from "../UI/imageCardForSearch/ImageCardForSearch";
 import classes from "./searchBook.module.css"
+import {AiFillStar, AiOutlineStar} from "react-icons/all";
+import Rating from "react-rating";
 
 class SearchBook extends React.Component {
 
     state = {
         keyword: '',
+        criteria: '',
+        additionalKeyword: '',
         books: []
     }
 
-    handleChange = (event) => {
+    handleKeywordChange = (event) => {
         this.setState(
             {keyword: event.target.value})
     }
 
+    handleAdditionalKeywordChange = (event) => {
+        this.setState(
+            {additionalKeyword: event.target.value})
+
+    }
+
+    handleCriteriaChange = (event) => {
+        this.setState(
+            {criteria: event.target.value})
+    }
+
     searchBook = () => {
-        const url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.keyword}`;
+        let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.keyword}`;
+        if (this.state.additionalKeyword.length > 0 && this.state.criteria.length > 0) {
+            url += "+" + this.state.criteria + ":" + this.state.additionalKeyword
+        }
         fetch(url).then(response => response.json()).then(this.renderBooks)
     }
     renderBooks = (response) => {
@@ -26,33 +44,70 @@ class SearchBook extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="input-group">
+            <div  className={`${classes.SearchBook}`}>
+                <div className={`input-group ${classes.searchBar}`}>
                     <input value={this.state.keyword}
-                           onChange={this.handleChange}
+                           onChange={this.handleKeywordChange}
                            className="form-control"
-                           placeholder="keyword"/>
+                           placeholder="Keyword..."/>
                     <div className="input-group-append">
                         <button
                             onClick={this.searchBook}
                             className="btn btn-primary">
-                            Search
+                            <i className="fa fa-search"/>
                         </button>
                     </div>
                 </div>
-                <div className={`${classes.SearchBook} row`}>
-                    {
+                <div className={`input-group ${classes.searchBar}`}>
+                    <input value={this.state.additionalKeyword}
+                           onChange={this.handleAdditionalKeywordChange}
+                           className="form-control"
+                           placeholder="Additional keyword (optional)..."/>
+                    <select className="custom-select"
+                            onChange={this.handleCriteriaChange}>
+                        <option selected>criteria</option>
+                        <option value="inauthor">author</option>
+                        <option value="subject">subject</option>
+                        <option value="isbn">isbn</option>
+                    </select>
+
+                </div>
+
+                <div className={`row`}>
+                    {   this.state.books &&
                         this.state.books.map(book =>
-                        <div className={`col-3`}>
-                            <div className={classes.searchBookResult}
-                              >
+                        <div className={`col-xl-3 col-lg-4 col-md-4 col-sm-12 col-xm-12`}>
+                            <div>
                                 <ImageCard
-                                    className={classes.searchBookResult}
                                     src={book.volumeInfo.imageLinks.thumbnail}/>
-                                    {/*<h1>Book</h1>*/}
                             </div>
-                        </div>
-                        )
+                            <div className={classes.bookTitle}>
+                                <div>
+                                    {
+                                        book.volumeInfo.title.length > 15 && book.volumeInfo.title.substring(0, 15)
+                                    }
+                                    {
+                                        book.volumeInfo.title.length > 15 && "..."
+                                    }
+                                    {
+                                        book.volumeInfo.title.length <= 15 && book.volumeInfo.title
+                                    }
+                                </div>
+
+                                <Rating initialRating={book.volumeInfo.averageRating} readonly
+                                        emptySymbol={<AiOutlineStar color="gold" className="mb-1"/>}
+                                        fullSymbol={<AiFillStar color="gold" className="mb-1"/>}/>
+                                <Link to={`/books/${book.id}`}
+                                      className={`btn btn-primary btn-block ${classes.detailButton}`}>
+                                    View More...
+                                </Link>
+                            </div>
+
+                        </div>)
+                    }
+                    {
+                        !this.state.books &&
+                        <h4>No results available, please search again...</h4>
                     }
                 </div>
             </div>
