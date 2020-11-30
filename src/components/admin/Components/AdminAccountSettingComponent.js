b08f8d9ba26d5c8d9eff5534f3444fbac6a8828b
Yewen Wu <wendy@Yewens-MacBook-Pro.local>
2020-11-29 23:38:44 -0800

    Changing loggedIn profile page styling.

--- src/components/admin/Components/AdminAccountSettingComponent.js
+++ src/components/admin/Components/AdminAccountSettingComponent.js
@@ -1,33 +1,31 @@
 import React from "react";
 
 const AdminAccountSettingComponent = () =>
-    <div>
-        <div>
-            <h2>Edit admin credential</h2>
-            <br/>
-            <div className="form-group row">
-                <label htmlFor="change-email" className="col-sm-3 col-form-label">
-                    Email
-                </label>
-                <div className="col-sm-9">
-                    <input id="change-email" className="form-control" placeholder="admin@admin.com"/>
-                </div>
-            </div>
-            <div className='form-group row'>
-                <label htmlFor="change-password" className="col-sm-3 col-form-label">
-                    Password
-                </label>
-                <div className="col-sm-9">
-                    <input type="password" className="form-control" id="change-password"/>
-                </div>
-
-            </div>
-            <div className="float-right">
-                <button className="btn btn-danger m-2">Cancel</button>
-                <button className="btn btn-success m-2">Save</button>
+    <form>
+        <h2>Edit admin credential</h2>
+        <br/>
+        <div className="form-group row">
+            <label htmlFor="change-email" className="col-sm-3 col-form-label">
+                Email
+            </label>
+            <div className="col-sm-9">
+                <input id="change-email" className="form-control" placeholder="admin@admin.com"/>
             </div>
         </div>
-    </div>
+        <div className='form-group row'>
+            <label htmlFor="change-password" className="col-sm-3 col-form-label">
+                Password
+            </label>
+            <div className="col-sm-9">
+                <input type="password" className="form-control" id="change-password"/>
+            </div>
+
+        </div>
+        <div className="float-right">
+            <button className="btn btn-danger m-2">Cancel</button>
+            <button className="btn btn-success m-2" type="submit">Save</button>
+        </div>
+    </form>
 
 
 export default AdminAccountSettingComponent
