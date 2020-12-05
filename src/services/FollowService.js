13ee3b81a0b9319daeaefb8e8a8c4403a63e22da
Guan Wang <guanwang0226@gmail.com>
2020-12-04 22:00:52 -0800

    Add Services

---
+++ src/services/FollowService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import authHeader from './AuthHeader';
+
+const API_URL = 'http://localhost:8080/api/users';
+
+class FollowService {
+
+    createFollow(userId, followingId){
+        return axios.post(`${API_URL}/${userId}/followings`,{"following": followingId}, {headers: authHeader()})
+    }
+
+    getAllFollowers(userId) {
+        return axios.get(`${API_URL}/${userId}/followers`, {headers: authHeader()})
+            .then((response) => (
+                response.data ? response.data : []
+            ))
+    }
+
+    getAllFollowings(userId) {
+        return axios.get(`${API_URL}/${userId}/followings`, {headers: authHeader()})
+            .then((response) => (
+                response.data ? response.data : []
+            ))
+    }
+
+    unfollow(userId, followingId) {
+        return axios.delete(`${API_URL}/${userId}/followings/${followingId}`, {headers: authHeader()})
+    }
+
+}
+
+export default new FollowService();
