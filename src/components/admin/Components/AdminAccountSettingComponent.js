a3ad42081bd4e08ffbc40b49243c25117ec1946e
Yewen Wu <wendy9956@gmail.com>
2020-12-05 15:37:56 -0800

    Adding form validation to admin account setting component.

--- src/components/admin/Components/AdminAccountSettingComponent.js
+++ src/components/admin/Components/AdminAccountSettingComponent.js
@@ -4,14 +4,22 @@ import UserActions from "../../../actions/userActions";
 import {Alert} from "reactstrap";
 import {Field, reduxForm} from "redux-form";
 
-
 class AdminAccountSettingComponent extends React.Component {
 
     state = {
         alertVisible: false
     }
 
-    renderInput = ({input, label, type, placeholder, id}) => {
+    renderError({error, touched}) {
+        if (touched && error) {
+            return (
+                <div className="alert alert-danger" role="alert">
+                    {error}
+                </div>)
+        }
+    }
+
+    renderInput = ({input, label, type, placeholder, id, meta}) => {
         return (
             <div className="form-group row">
                 <label className="col-sm-3 col-form-label" htmlFor={id}>
@@ -19,6 +27,7 @@ class AdminAccountSettingComponent extends React.Component {
                 </label>
                 <div className="col-sm-9">
                     <input {...input} className="form-control" type={type} placeholder={placeholder} id={id}/>
+                    {this.renderError(meta)}
                 </div>
             </div>
         )
@@ -30,8 +39,7 @@ class AdminAccountSettingComponent extends React.Component {
         if (formValues.password) {
             user.password = formValues.password;
         }
-        console.log(user)
-        this.props.dispatch(UserActions.updateUser(formValues._id, user))
+        this.props.dispatch(UserActions.updateUser(this.props.initialValues._id, user))
             .then(this.setState({alertVisible: true}, () => {
                 window.setTimeout(() => {
                     this.setState({alertVisible: false})
@@ -49,20 +57,38 @@ class AdminAccountSettingComponent extends React.Component {
                        isOpen={this.state.alertVisible}>
                     Success updating your account!
                 </Alert>
-                <form onSubmit={handleSubmit(this.onSubmit)} >
+                <form onSubmit={handleSubmit(this.onSubmit)}>
                     <br/>
                     <Field name="email" component={this.renderInput} label="Email*" type="email"
                            placeholder="user@user.com" id="email"/>
                     <Field name="password" component={this.renderInput} label="Password" type="password"
                            placeholder="Enter New Password" id="password"/>
                     <div className="float-right">
-                        <button className="btn btn-danger m-2" type="button" disabled={pristine || submitting} onClick={reset}>Reset</button>
-                        <button className="btn btn-success m-2" type="submit" disabled = {pristine || submitting}>Save</button>
+                        <button className="btn btn-danger m-2" type="button" disabled={pristine || submitting}
+                                onClick={reset}>Reset
+                        </button>
+                        <button className="btn btn-success m-2" type="submit" disabled={pristine || submitting}>Save
+                        </button>
                     </div>
                 </form>
             </div>
         )
     }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+    const {email, password} = formValues;
+    if (!email) {
+        errors.email = "This field is required!"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+        errors.email = "Invalid email address!"
+    }
+
+    if (password && (password.length < 6 || password.length > 40)) {
+        errors.password = "The password must be between 6 and 40 characters."
+    }
+    return errors;
 }
 
 const stateToPropertyMapper = (state) => ({
@@ -71,7 +97,8 @@ const stateToPropertyMapper = (state) => ({
 
 export default connect(stateToPropertyMapper)(reduxForm({
     form: 'adminAccountSettingComponent',
-    enableReinitialize : true
+    validate,
+    enableReinitialize: true,
 })(AdminAccountSettingComponent));
 
 
