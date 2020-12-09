d268318af4f0b03196cce4259ba7cec2797043b9
April <38486418+ricezhang11@users.noreply.github.com>
2020-12-09 13:02:15 -0800

    added null check to prevent invalid profile url  (#93)
    * fixed check null issue in borrowing component
    
    * fixed dropdown menu, use onChange
    
    * added auth for profile page, fixed some bugs related to following component
    
    * added auth to componentdidmount
    
    * fixed following component
    
    * fixed auth related to profile page and reading list
    
    * fixed bug in following
    
    * fixed links to books
    
    * added missing book links
    
    * fixed book links
    
    * fixed book title format
    
    * fixed wrong link to user profile
    
    * fixed image card format
    
    * changed email
    
    * added rating number, fixed unfollow and book link in admin table
    
    * added followings in public profile
    
    * fixed bug in public profile followings
    
    * fixed bug with profile image
    
    * fixed bug with profile photo
    
    * fixed details
    
    * changed wrong headings
    
    * fix admin repeated delete the same review
    
    * fixed repeatedly delete review bug in admin page
    
    * changed profile page routes
    
    * fixed logout to public profile
    
    * fixed logout to public profile
    
    * cleaned logic
    
    * additional changes
    
    * prevent invalid call to profile

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -13,7 +13,6 @@ import {
     fetchFollowers,
     fetchFollowings,
     fetchLoggedInUserFollowings,
-    authenticate
 } from "../../actions/profileActions";
 import UserActions from "../../actions/userActions";
 
@@ -22,7 +21,6 @@ class UserProfile extends React.Component {
     componentDidMount() {
         const uid = this.props.match.params.userId
         const section = this.props.match.params.section
-        this.props.authenticate(uid, this.props.loggedInUser, this.props.isLoggedIn)
         if ((this.props.isLoggedIn && !uid) || (this.props.isLoggedIn && this.props.loggedInUser._id === uid)) {
             this.props.findUserById(this.props.loggedInUser._id)
             this.props.fetchBookPostingsForUser(this.props.loggedInUser._id)
@@ -57,7 +55,6 @@ class UserProfile extends React.Component {
         const uid = this.props.match.params.userId
         const section = this.props.match.params.section
         if (section !== prevProps.match.params.section || uid !== prevProps.match.params.userId) {
-            this.props.authenticate(uid, this.props.loggedInUser, this.props.isLoggedIn)
             if ((this.props.isLoggedIn && !uid) || (this.props.isLoggedIn && this.props.loggedInUser._id === uid)) {
                 this.props.findUserById(this.props.loggedInUser._id)
                 this.props.fetchBookPostingsForUser(this.props.loggedInUser._id)
@@ -92,15 +89,24 @@ class UserProfile extends React.Component {
     render() {
         return (
             <div className="container">
-                {this.props.isLoggedIn && this.props.loggedInUser._id === this.props.user._id ?
-                <LoggedInProfile/> : <PublicProfile/>}
+                {
+                    Object.keys(this.props.user).length === 0 && this.props.user.constructor === Object
+                    && <span>"Invalid User ID"</span>
+                }
+                {
+                    !(Object.keys(this.props.user).length === 0 && this.props.user.constructor === Object) && this.props.isLoggedIn && this.props.loggedInUser._id === this.props.user._id &&
+                        <LoggedInProfile/>
+                }
+                {
+                    !(Object.keys(this.props.user).length === 0 && this.props.user.constructor === Object)  && !(this.props.isLoggedIn && this.props.loggedInUser._id === this.props.user._id) &&
+                        <PublicProfile/>
+                }
             </div>
         )
     }
 }
 
 const stateToPropertyMapper = (state) => ({
-    authenticated: state.profile.authenticated,
     loggedInUser: state.auth.user,
     isLoggedIn: state.auth.isLoggedIn,
     user: state.profile.user
@@ -119,5 +125,4 @@ export default connect(stateToPropertyMapper,
         fetchFollowings,
         fetchLoggedInUserFollowings,
         getReadingListForUser,
-        authenticate,
     })(UserProfile)
