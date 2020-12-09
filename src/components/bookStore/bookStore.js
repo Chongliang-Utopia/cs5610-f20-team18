e3bec82241f411a4ff0fcd654606297a5dc292da
Chongliang-Utopia <50967591+Chongliang-Utopia@users.noreply.github.com>
2020-12-09 12:05:00 -0500

    updated bookstore to encode url with search keywords

--- src/components/bookStore/bookStore.js
+++ src/components/bookStore/bookStore.js
@@ -3,17 +3,37 @@ import classes from "./filterPanel.module.css"
 import SearchBook from "./searchBook";
 import FilterPanel from "./FilterPanel"
 import {connect} from "react-redux";
-import {searchBook} from "../../actions/searchBookActions"
-import {getRecommendedBooks} from "../../actions/searchBookActions";
+import {getRecommendedBooks, setBookSearchCriteria, setSearchDefaultTerm,
+    searchBookByCriteria, clearSearchKeywords} from "../../actions/searchBookActions";
 
 class BookStore extends React.Component {
     componentDidMount() {
-        if (this.props.search_default_term) {
-            this.props.searchBook(this.props.search_default_term)
+        // if (this.props.search_default_term) {
+        //     this.props.searchBook(this.props.search_default_term)
+        // } else {
+        //     this.props.getRecommended();
+        // }
+        if (this.props.match.params.criteria) {
+            this.props.setSearchDefaultTerm(this.props.match.params.criteria);
+            this.props.searchBookByCriteria(this.props.match.params.criteria);
         } else {
             this.props.getRecommended();
+            this.props.clearSearchKeywords();
         }
     }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.match.params.criteria !== this.props.match.params.criteria) {
+            if (this.props.match.params.criteria) {
+                this.props.setBookSearchCriteria(this.props.match.params.criteria);
+                this.props.searchBookByCriteria(this.props.match.params.criteria);
+            } else {
+                this.props.getRecommended();
+                this.props.clearSearchKeywords();
+            }
+        }
+    }
+
+
     render() {
         return (
             <div className={`${classes.BookStore} container`}>
@@ -33,12 +53,17 @@ class BookStore extends React.Component {
 }
 const stateToPropertyMapper = (state) => ({
     search_default_term: state.searchBookReducer.search_default_term,
+    criteria: state.searchBookReducer.criteria
 })
 
 const propertyToDispatchMapper = (dispatch) => ({
-    searchBook: (search_default_term, author, title, isbn, publisher, subject) =>
-        searchBook(dispatch, search_default_term, author, title, isbn, publisher, subject),
-    getRecommended: () => getRecommendedBooks(dispatch)
+    // searchBook: (search_default_term, author, title, isbn, publisher, subject) =>
+    //     searchBook(dispatch, search_default_term, author, title, isbn, publisher, subject),
+    getRecommended: () => getRecommendedBooks(dispatch),
+    setBookSearchCriteria: (criteria) => setBookSearchCriteria(dispatch, criteria),
+    searchBookByCriteria: (criteria) => searchBookByCriteria(dispatch, criteria),
+    clearSearchKeywords: () => clearSearchKeywords(dispatch),
+    setSearchDefaultTerm: (term) => setSearchDefaultTerm(dispatch, term)
 })
 export default connect(stateToPropertyMapper, propertyToDispatchMapper)
 (BookStore)
